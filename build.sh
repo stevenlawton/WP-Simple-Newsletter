c6ddc186d93431fa87de6b0987ffcc24ab7c585a
Steve Lawton <steve@acewebservices.co.uk>
2024-09-03 20:41:07 +0100

    1st commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r "wp-simple-newsletter.zip" "simple-newsletter.php" "recaptcha-config.php" "hero-section-signup.php" "snl-hero-block.js" "snl-hero-block.css"
---
+++ recaptcha-config.php
@@ -0,0 +1,7 @@
+<?php
+// Google reCAPTCHA Configuration
+
+define('RECAPTCHA_SITE_KEY', '');
+define('RECAPTCHA_SECRET_KEY', '');
+
+define('SNL_HERO_BACKGROUND','/wp-content/uploads/2019/10/dnh30-222.jpg');
